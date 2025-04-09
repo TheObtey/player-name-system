436cf03ee9e5f4391675d8defdf5ce68e049b74d
unknown <justobtey@gmail.com>
2025-04-09 14:12:05 +0200

    Added base module structure and init file

---
+++ lua/autorun/theobtey-player-name-system.lua
@@ -0,0 +1,15 @@
+TOPNS = TOPNS or {}
+
+if SERVER then
+    
+    include("core/storage.lua")
+    include("core/name_validator.lua")
+    include("core/name_handler.lua")
+
+end
+
+if CLIENT then
+    
+
+
+end
